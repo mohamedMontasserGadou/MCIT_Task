848314e780d3daea41a16944a128012c8e997e3f
m.gadou <m.gadou@EGY-PMT-EMP-26>
2021-05-10 00:54:01 +0200

    Adding UOW

---
+++ Infrastructure/UOW/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using MCIT_Task.Infrastructure.Repostiories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCIT_Task.Infrastructure.UOW
+{
+    public interface IUnitOfWork
+    {
+        public GenericRepository<TEntity> GetRepo<TEntity>() where TEntity : class;
+        public Task CompleteAsync();
+    }
+}
---
+++ Infrastructure/UOW/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using MCIT_Task.Infrastructure.DBContext;
+using MCIT_Task.Infrastructure.Repostiories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCIT_Task.Infrastructure.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public GenericRepository<TEntity> GetRepo<TEntity>() where TEntity : class
+        {
+            return new GenericRepository<TEntity>(_dbContext);
+        }
+    }
+}
