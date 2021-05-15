b1240b27d055f826655de7c3ca09db0cdd8657ca
m.gadou <m.gadou@EGY-PMT-EMP-26>
2021-05-15 11:08:09 +0200

    Adding register and _forms shared compoenet

--- Angualr/src/app/app.module.ts
+++ Angualr/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
