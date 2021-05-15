100679be502d71de5c4d6f23563f9a5ccc3cab08
m.gadou <m.gadou@EGY-PMT-EMP-26>
2021-05-15 11:36:41 +0200

    adding home component

--- Angualr/src/app/app.module.ts
+++ Angualr/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     TextInputComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
